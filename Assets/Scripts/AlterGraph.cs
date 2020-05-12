edd8dd2db91194df2b1e9fba1da8eec3be890ad6
Scott Cabot <iam@scott.cab>
2020-05-12 19:16:24 +0930

    Added initial prject scaffold.

---
+++ Assets/Scripts/AlterGraph.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlterGraph : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteMask mask;
+    
+    private void Update()
+    {
+        mask.alphaCutoff = 1 - Input.GetAxis("A");
+    }
+}
