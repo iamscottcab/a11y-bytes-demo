6e96301eac4f8fd27c2f2021e9c3ff150db09d2c
Scott Cabot <iam@scott.cab>
2020-05-13 08:30:50 +0930

    Added ButtonDown logic.

--- Assets/Scripts/AlterGraph.cs
+++ Assets/Scripts/AlterGraph.cs
@@ -6,9 +6,43 @@ public class AlterGraph : MonoBehaviour
 {
     [SerializeField]
     private SpriteMask mask;
-    
+
+    [SerializeField]
+    private GameObject label;
+
+    private bool showingLabel = false;
+
+    private bool canShowLabel = false;
+
+    private void Awake()
+    {
+        label.SetActive(false);
+    }
+
     private void Update()
     {
         mask.alphaCutoff = 1 - Input.GetAxis("A");
+
+        if (canShowLabel && Input.GetKeyDown(KeyCode.Joystick1Button0))
+        {
+            if (!showingLabel)
+            {
+                StartCoroutine(ShowLabel());
+            }
+        }
+    }
+
+    private IEnumerator ShowLabel()
+    {
+        showingLabel = true;
+        label.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        label.SetActive(false);
+        showingLabel = false;
+    }
+
+    private void OnGUI()
+    {
+        canShowLabel = GUILayout.Toggle(canShowLabel, "  -  Show Pressed State.");
     }
 }
